14eabcef16251f67acbbc9c5c63c06910db49ece
Nicnide12 <nicnidecolec@gmail.com>
2025-09-04 23:28:47 -0400

    Add files via upload

---
+++ Monportfolio.js
@@ -0,0 +1,6 @@
+let maCase = document.getElementById('maCaseACocher');
+document.getElementById('bouton-newsletter').addEventListener('click', function() {
+  // Ici, vous déclencherez l'action d'inscription.
+  // Cela peut être l'affichage d'un popup ou l'appel d'une fonction.
+  alert('Merci de vous être abonné à notre newsletter !'); // Exemple simple avec une alerte
+});
